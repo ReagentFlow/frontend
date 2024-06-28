77d18c754cecf094bd019d92ff040553d406efc3
quzor <egor.khantakov@gmail.com>
2024-06-28 18:34:51 +0700

    формы и стили для логина и регистрации

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Login from './components/Login';
+import Register from './components/Register';
 import Home from './components/Home';
 import ReagentsTable from './components/ReagentsTable';
 import WeatherTable from './components/WeatherTable';
@@ -14,6 +16,8 @@ function App() {
             <main>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                     <Route path="/reagents" element={<ReagentsTable />} />
                     <Route path="/weather" element={<WeatherTable />} />
                     <Route path="/settings" element={<Settings />} />
